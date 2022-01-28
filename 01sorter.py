c3bf5834d63d061c0c5a8e22f2bace6f02007b31
Ajay Kumar Reddy Arram <77767099+ajayarram@users.noreply.github.com>
2022-01-28 14:59:11 -0600

    output files added

---
+++ 01sorter.py
@@ -0,0 +1,10 @@
+file_open = open("Arram.txt","r")  # open file, read-only
+file_sorted = open("sorted_file.txt", "w") # open file, write
+lines = file_open.readlines()
+lines.sort()
+
+for line in lines:
+ file_sorted.write(line)
+
+file_open.close()
+file_sorted.close()
